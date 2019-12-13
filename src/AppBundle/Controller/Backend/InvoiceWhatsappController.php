e419079ecc2b4d910962cd8e69fbf8d785b45d5b
Nelson Castillo <ncastillo@ebclosion.com>
2019-12-13 16:36:34 -0400

    NC

--- src/AppBundle/Controller/Backend/InvoiceWhatsappController.php
+++ src/AppBundle/Controller/Backend/InvoiceWhatsappController.php
@@ -37,9 +37,6 @@ class InvoiceWhatsappController extends Controller
      */
     public function indexAction (Request $request)
     {
-        $k1 = new \DateTime();
-        print_r($k1);
-        die;
         $this->get("session")->set("module_id", $this->moduleId);
 
         $mp = EbClosion::getModulePermission($this->moduleId,

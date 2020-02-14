93b77cfd4df51149205e5960c3f98cc60a187321
Rodrigo Guerra <rguerra@ebclosion.com>
2020-02-14 15:18:54 -0400

    web y registro de partcipantes

--- src/AppBundle/Controller/Frontend/WebsiteController.php
+++ src/AppBundle/Controller/Frontend/WebsiteController.php
@@ -10,6 +10,7 @@ use AppBundle\Form\StaffLoginType;
 use AppBundle\Form\StaffType;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use AppBundle\Entity\RegisterPending;
+use AppBundle\Helper\ApiHelper;
 
 class WebsiteController extends Controller
 {
@@ -19,176 +20,11 @@ class WebsiteController extends Controller
      * @Route("/", name="frontend_index")
      */
     public function landingAction (Request $request)
-    {
-        $form = $this->createForm(new StaffType());
-        $form->handleRequest($request);
-       
-        $em = $this->getDoctrine()->getManager();
-       // $promotions = $em->getRepository("AppBundle:Staff")->getAllCampaingPromotions();
-       
-        return $this->render('@App/Frontend/Website/index.html.twig',
-                array(
-                        "form" => $form->createView(), 
-                        //"promotions" => $promotions,
-                ));
+    {       
+
+        return $this->render('@App/Frontend/Website/index.html.twig',  array());
     }
 
-    
-    /**
-     *
-     * @Route("/register", name="frontend_register_user")
-     */
-    public function registerUserAction (Request $request)
-    {
-        $dpi = $request->get("dpi");
-        $phone = $request->get("phone");
-        $name = $request->get("name");
-        $lastName = $request->get("lastName");
-        $countryId = $request->get("country");
-        $email = $request->get("email", false);
-
-        $staff = $this->getDoctrine()
-            ->getRepository('AppBundle:Staff')
-            ->findOneBy(array(
-                "citizenId" => $dpi,
-                "phone" => $phone,
-                "country" => $countryId
-            ));
-
-        if ($staff)
-        {
-            $response = array('status'=>'success', 'message' =>'El usuario ya se encuentra registrado');
-            return new JsonResponse($response);
-        } else{                       
-
-            $em = $this->getDoctrine()->getManager();
-            
-            $country = $em->getRepository("AppBundle:Country")->findOneBy(array("id" => $countryId));
-            
-            $staff = new Staff();
-            $staff->setName($name . " " . $lastName);
-            $staff->setCitizenId($dpi);
-            $staff->setPhone($phone);
-            $staff->setCreatedAt(new \DateTime());
-            $staff->setCountry($country);
-            $staff->setEmail($email);
-
-            $em->persist($staff);
-            $em->flush();
-
-            $this->get("session")->set("staff", $staff);
-            
-            $store = $this->getDoctrine()
-            ->getRepository('AppBundle:Store')->findBy(array("country" => $country));
-            $arrayStore = [];
-            foreach ($store as $item) {
-                array_push($arrayStore, array("store_id" => $item->getId(), "name" => $item->getStoreName()));
-            }
-            
-            $response = array('status'=>'success', "store_list" => $arrayStore, "sid" => md5($staff->getStaffId()));        
-            
-        }    
-        
-        							
-		return new JsonResponse($response);
-    }
-
-    /**
-     *
-     * @Route("/register_login", name="frontend_login_user")
-     */
-    public function loginUserAction (Request $request)
-    {
-
-        $dpi = $request->get("dpi");
-        $phone = $request->get("phone");
-        $country = $request->get("country");
-
-        $staff = $this->getDoctrine()
-            ->getRepository('AppBundle:Staff')
-            ->findOneBy(array(
-                "citizenId" => $dpi,
-                "phone" => $phone,
-                "country" => $country
-            ));
-
-        if ($staff) {
-            
-            // get stores
-            $store = $this->getDoctrine()
-            ->getRepository('AppBundle:Store')->findBy(array("country" => $country));
-            $arrayStore = [];
-            foreach ($store as $item) {
-                array_push($arrayStore, array("store_id" => $item->getId(), "name" => $item->getStoreName()));                               
-            }
-            
-            $this->get("session")->set("staff", $staff);
-            $response = array('status'=>'success', "store_list" => $arrayStore, "sid" => md5($staff->getStaffId()));            
-        }
-        else    
-            $response = array('status'=>'success', 'message' =>'El usuario no existe');
-        
-		return new JsonResponse($response);
-    }
-    
-    /**
-     *
-     * @Route("/register_code", name="frontend_register_code")
-     */
-    public function registerCodeAction (Request $request)
-    {
-        
-        $code = $request->get("code", false);
-        $invoice = $request->get("invoice", false);
-        $store = $request->get("store", false);
-        $sid = $request->get("sid", false);              
-        
-        
-        
-        if ($code && $invoice && $store && $sid) {
-            
-            $staff = $this->getDoctrine()
-            ->getRepository('AppBundle:Staff')
-            ->findByMd5Id($sid);
-            
-            $response = $this->forward('AppBundle\Controller\Webservices\IndexController::registeCodeAction', [
-                  "staff_id" =>$staff["staff_id"],
-                   "code" => $code,
-                   "store_id" => $store,
-                   "bill_number" => $invoice
-            ]);
-            
-            $result = json_decode($response->getContent(), true);
-            
-            if ($result["status"] == "ok") {
-                $response = array('error'=>'success');
-            } else {
-                $response = array('error'=>'success', "message" => $result["code"]);
-            }
-            
-            
-            
-        }
-        else
-            $response = array('status'=>'success', 'message' =>'Ocurrio un error al validar los datos, por favor intentelo de nuevo.');
-            
-        return new JsonResponse($response);
-    }
-
-    /**
-     *
-     * @Route("/", name="frontend_index_initial")
-     */
-    public function indexAction (Request $request)
-    {
-
-        // replace this example code with whatever you need
-        return $this->render('@App/Frontend/Website/index.html.twig',
-                array(
-                        //"form" => $form->createView(),
-                        //"siteContent" => $siteContent
-                ));
-    }
 
     /**
      *
@@ -198,6 +34,82 @@ class WebsiteController extends Controller
     {
         // replace this example code with whatever you need
         return $this->render('@App/Frontend/Website/about.html.twig', array());
+    }
+
+    /**
+     *
+     * @Route("/register", name="frontend_register")
+     */
+    public function registerStaffAction (Request $request)
+    {
+        $apiHelper = new ApiHelper();	
+
+        $form = $request->get('data');
+
+        $register = false;
+        $msg = null;
+        $status = null;
+
+        if(isset($form)){
+
+            $register = true;
+
+            $firstName = $form['first_name'];
+			$lastName = $form['last_name'];
+			$email = $form['email'];
+			$citizenId = $form['citizen_id'];
+			$phone = $form['phone'];
+            $country = $form['country'];
+            
+            $postdata = json_encode(
+				array(
+					"first_name" => $firstName,
+					"last_name" => $lastName,
+					"email" => $email,
+					"citizen_id" => $citizenId,
+					"phone" => $phone,
+					"country" => $country
+				)
+            );
+            
+            $insert = $apiHelper->connectServices("http://localhost/contenedor_4/web/app_dev.php/ws/register-staff", "POST", null, $postdata);
+			$insert = json_decode($insert);
+
+			if($insert->status == 'success'){
+                $msg = $insert->msg;
+                $status = "success";
+			} else {
+                $msg = $insert->msg;
+                $status = "error";
+			}
+        }
+
+        $coutries = $apiHelper->connectServices("http://localhost/contenedor_4/web/app_dev.php/ws/get-countries", "GET", null, null);
+        $coutries = json_decode($coutries);
+        
+        if($coutries->status == 'success'){
+			$list = $coutries->data;
+		} else {
+			$list = null;
+        }
+
+        // replace this example code with whatever you need
+        return $this->render('@App/Frontend/Website/register.html.twig', array(
+            "countries" => $list,
+            "register" => $register,
+            "status" => $status,
+            "msg" => $msg
+        ));
+    }
+
+    /**
+     *
+     * @Route("/login", name="frontend_login")
+     */
+    public function loginStaffAction (Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('@App/Frontend/Website/login.html.twig', array());
     }
 
     
